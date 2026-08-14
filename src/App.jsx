6f2cb2cd88084760352dd2ae1365437cfb3d69ad
Cecilia Stier <cstier@lakecountypress.com>
2026-08-14 16:52:07 -0500

    added empty values for dropdowns

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import Tabs from './components/Tabs'
 import FileInput from './components/FileInput'
 
 const PROJECT_TYPE = [
-  { label: 'Select One', value: '' },
+  { label: 'Select One...', value: '' },
   { label: 'Shrinkwrap Only', value: 'shrinkwrap-only' },
   { label: 'Kit Assembly', value: 'kit-assembly' },
   { label: 'Kit Assembly with Drop Shipments', value: 'kit-assembly-drop-ship' },
@@ -24,6 +24,7 @@ const PROJECT_TYPE = [
 ]
 
 const TIMELINE_OPTIONS = [
+  { label: 'Select the desired timeline', value: '' },
   { label: 'As soon as possible', value: 'As soon as possible' },
   { label: '1-3 months', value: '1-3 months' },
   { label: '3-6 months', value: '3-6 months' },
@@ -31,12 +32,14 @@ const TIMELINE_OPTIONS = [
   { label: 'Flexible', value: 'Flexible' }
 ]
 const CARTON_TYPE = [
+  { label: 'Select Carton Type', value: '' },
   { label: 'Convenient/Standard', value: 'convenient' },
   { label: 'Double Walled', value: 'double-walled' },
   { label: 'Custom', value: 'custom' },
 ]
 
 const SHIPPING_METHOD = [
+  { label: 'Select Shipping Method', value: '' },
   { label: 'Ground', value: 'ground' },
   { label: '2-Day', value: '2-day' },
   { label: 'Overnight', value: 'overnight' },
@@ -375,7 +378,7 @@ function App() {
             name="ShippingMethod"
             value={shippingMethod}
             onChange={(e) => setShippingMethod(e.target.value)}
-            options={PROJECT_TYPE}
+            options={SHIPPING_METHOD}
           />
           <CheckboxInput
             label='Advanced Shipping Notice (ASN) Required'
